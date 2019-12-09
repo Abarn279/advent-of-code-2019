93b1947f48f2efbd28834df3108b7ae73ae885ed
Andrew Barnett <Abarn279@gmail.com>
2019-12-09 12:24:55 -0600

    really part b?

---
+++ src/_9b.py
@@ -0,0 +1,9 @@
+from file_importer import FileImporter
+from intcode_computer import IntcodeComputer
+
+prog = list(map(int, FileImporter.get_input("/../input/9.txt").split(",")))
+
+computer = IntcodeComputer(prog, [2])
+
+while not computer.halted:
+    print(computer.get_output())
