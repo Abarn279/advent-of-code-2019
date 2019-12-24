056f012fbe361aa40b67ff786b1d8b5ecc74b500
Andrew Barnett <Abarn279@gmail.com>
2019-12-24 10:25:38 -0600

    24

--- src/_24b.py
+++ src/_24b.py
@@ -6,10 +6,6 @@ DIRECTIONS = [Vector2(0, 1), Vector2(0, -1), Vector2(1, 0), Vector2(-1, 0)]
 
 n_adjacent = lambda grid, pos: sum(1 for d in DIRECTIONS if grid[pos + d] == '#')
 
-get_biod_r = lambda st: sum(2 ** idx for idx, item in enumerate(st) if item == '#')
-
-get_hashable = lambda grid: "".join([grid[Vector2(x, y)] for y in range(5) for x in range(5)])
-
 # Input 
 inp = FileImporter.get_input("/../input/24.txt").split("\n")
 grid = defaultdict(lambda: ' ')
@@ -17,9 +13,6 @@ for y in range(len(inp)):
     for x in range(len(inp[y])):
         grid[Vector2(x, y)] = inp[y][x]
 
-# Algo
-layouts = set()
-layouts.add(get_hashable(grid))
 while True:
     n_grid = grid.copy()
     keys = list(grid.keys())
